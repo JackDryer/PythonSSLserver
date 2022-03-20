1414d36a6bb6336548dee4a4a7d7d7711224d160
JackDryer <73561431+JackDryer@users.noreply.github.com>
2022-03-20 16:38:45 +0000

    Added files to GitHub for the first time

---
+++ client.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python3
+from sys import argv, modules
+if 'idlelib.run' in modules: #if the file is runs from idle it runs as a pyw
+    from subprocess import call
+    print ("by defult .pyws are blocked by the firewall, opening as a .py")
+    call(f"python {argv[0]}")
+
+import socket
+import threading
+import tkinter as tk
+from tkinter import scrolledtext, filedialog
+import json
+import ssl
+import time
+
+HEADERSIZE = 16
+BUFFERSIZE = 64
+ENCODING = "utf-8"
+SERVERIP = input("please enter servername")
+if not SERVERIP:
+    SERVERIP= socket.gethostname()
+PORT = 5050
+ADRS = (SERVERIP,PORT)
+
+SERVERPREF = "/"
+CLIENTPREF = "!"
+FILEPREF = "%"
+FULLLOGGGING= False
+
+class client(object):
+    def __init__(self,ADRS):
+        ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        ctx.load_verify_locations("servercert.pem")
+        ctx.check_hostname  = False
+        self.ADRS = ADRS
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = ctx.wrap_socket(self.sock,server_hostname=SERVERIP)
+        self.sock.connect((ADRS))
+
+    
+    def send(self,msg):
+        if type(msg) == str:
+            msg = self.parsestring(msg)
+        if "clientcommand" in msg:
+            if not "cargs" in msg:
+                msg["cargs"]= []
+            if not "ckwargs" in msg:
+                msg["ckwargs"]= {}
+        if "servercommand" in msg:
+            if not "sargs" in msg:
+                msg["sargs"]= []
+            if not "skwargs" in msg:
+                msg["skwargs"]= {}
+        msg["time"] = time.time()# used to generate ids
+        msg = json.dumps(msg)
+        msg = self.encode(msg)
+        if FULLLOGGGING: print(msg)
+        self.sock.send(msg)
+
+    @classmethod
+    def encode(cls,msg):
+        msg = bytes(msg,ENCODING)
+        while True:
+            header = bytes(f"{len(msg):<{HEADERSIZE}}",ENCODING)
+            if len(header) > HEADERSIZE:
+                header = cls.encode(json.dumps({"largefile":header}),ENCODING)
+            return header + msg
+    @classmethod
+    def parsestring(self,string):
+        out = {}
+        if string[0] == "/":
+            command = string.split()
+            out["servercommand"] =command[0][1:]
+            if len (command)>1:
+                out["sargs"] = command[1:]
+            else:
+                out["sargs"] = None
+        elif string[0] == "!":
+            command = string.split(">")
+            out["clientcommand"] =command[0][1:]
+            if len (command)>1:
+                out["cargs"] = command[1:]
+            else:
+                out["cargs"] = []
+        else:
+            out["message"]  = string
+        return out
+
+    def recivebytes(self,msgsize):
+        if msgsize <BUFFERSIZE:
+            fullmsg = self.sock.recv(msgsize)
+        else:
+            fullmsg =  b''
+            for i in range(int(msgsize/BUFFERSIZE)):
+                fullmsg += self.sock.recv(BUFFERSIZE)
+            fullmsg += self.sock.recv(msgsize%BUFFERSIZE)
+        return fullmsg
+    
+    def recive(self):
+        header = self.recivebytes(HEADERSIZE)
+        msg =self.recivebytes(int(header.decode(ENCODING)))
+        return msg
+        
+    def decode(self,msg):
+        if FULLLOGGGING: print(msg)
+        msg = msg.decode(ENCODING)
+        msg = json.loads(msg)
+        if "largefile" in msg:
+            msg = decode(recivebytes(int(msg["largefile"])))
+        if "clientcommand" in msg:
+            cmd = msg["clientcommand"]
+            if cmd == "stream":
+                streamrcv = streamrecvier(self.master,*msg["cargs"],**msg["ckwargs"])
+                msg["message"] = f"connecting to {msg['sender']}"
+            elif cmd == "Disconnect":
+                msg["message"] ="!Disconnect"
+        elif "message" in msg:
+            
+            ##msg = f'{msg["sender"]}> {msg["message"]}'
+            pass # message fomrating is now handeled by the UI
+        return msg
+
+    def disconnect(self):
+        self.send("/disconnect")
+
+
+
+
+
+
+
+
+class streamrecvier(client):           
+    def __init__(self,master,ip,port):
+        self.master = master
+        popup = tk.Toplevel(self.master)
+        conectionl = tk.Label(popup, text=f"connecting to {ADRS}")
+        conectionl.pack()
+        ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        ctx.load_verify_locations("clientkey.pem")
+        ctx.check_hostname  = False
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = ctx.wrap_socket(self.sock,server_hostname=ip)
+        self.sock.connect((ip,port))
+        recivingthread =threading.Thread(target = self.handlestream)
+        recivingthread.start()
+        popup.destroy()
+
+    def handlestream(self):
+        msg = self.recive()
+        msg = self.decode(msg)
+        if "streamcmd" in msg:
+            if msg["streamcmd"] == "writefile":
+                streaminfo = tk.Toplevel()
+                fileinfo = tk.Label(streaminfo,text= f'Downloading "{msg["filename"]}" ({msg["filesize"]} Bytes)')
+                fileinfo.pack()
+                self.recivefile(msg["filename"],int(msg["filesize"]))
+                streaminfo.destroy()
+    
+    def recivefile(self,filename, filesize):
+        with open("Recived Files/"+filename,"wb") as file:
+                for i in range(int(filesize/BUFFERSIZE)):
+                    file.write(self.sock.recv(BUFFERSIZE))
+                file.write(self.sock.recv(filesize%BUFFERSIZE))
+
+
+
+
+
+
+class streamhost(object):
+    def __init__(self,master,func,*funcargs,**funckwargs):
+        setupwindow = tk.Toplevel()
+        tk.Message(setupwindow, text="setting up stream").pack()
+        self.master = master
+        self.func = func
+        self.funcargs = funcargs
+        self.funckwargs = funckwargs
+        self.ip = socket.gethostname()
+        ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        ctx.load_cert_chain("clientkey.pem")
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server= ctx.wrap_socket(self.server,server_side = True)
+        for self.port in range (5100,5200):
+            try:
+                self.server.bind((self.ip,self.port))
+                break
+            except:
+                pass
+        streamthread = threading.Thread(target = self.handle)
+        streamthread.start()
+        setupwindow.destroy()
+
+    def handle(self):
+        streaminfo = tk.Toplevel(self.master)
+        streamlog = scrolledtext.ScrolledText(streaminfo)
+        streaming = True
+        def close():
+            streaming = False
+            self.server.shutdown(-1)
+            self.server.close()
+            streaminfo.destroy()
+        stopbutton = tk.Button(streaminfo,text = "stop streaming",command = close)
+        streamlog.pack()
+        stopbutton.pack()
+        while streaming:
+            self.server.listen(5)
+            client,address= self.server.accept()
+            clientcon= clientconnection(streamlog,client,address)
+            clienthandler = threading.Thread(target = self.func, args = (clientcon,*self.funcargs), kwargs = self.funckwargs)
+            clienthandler.start()
+
+class clientconnection:
+    def __init__(self,output,client,address):
+        self.output = output
+        self.client = client
+        output.insert(tk.INSERT,f"streaming to {address}\n")
+    
+    def sendfile(self,path):
+        name = path.split("/")[-1]
+        import os
+        filesize = os.path.getsize(path)
+        with open(path,"rb") as file:
+            self.client.send(client.encode(json.dumps({"streamcmd":"writefile","filename":name,"filesize":filesize})))
+            for i in range(int(filesize/BUFFERSIZE)):
+                self.client.send(file.read(BUFFERSIZE))
+            self.client.send(file.read(filesize%BUFFERSIZE))
+        self.client.close()
+        self.output.insert(tk.INSERT,"finnished\n")
+
+
+class ui(client):
+    def __init__(self,master,ADRS):
+        client.__init__(self,ADRS)
+        self.master = master
+        self.window = tk.Frame(master)
+        self.window.pack(fill = "both",expand = True)
+        self.messages = scrolledtext.ScrolledText(self.window)
+        self.inputfield = tk.Entry(self.window)
+        self.filebutton = tk.Button(self.window,text = "send file",command = self.sendfile)
+        self.inputfield.bind("<Return>", self.sendinput)
+        self.master.protocol("WM_DELETE_WINDOW", self.close)
+        self.inputfield.bind("<FocusOut>",self.createfillertext)
+        self.inputfield.bind("<FocusIn>",self.removefillertext)
+        self.messages.grid(row = 0, column = 0, sticky = "NSEW", rowspan = 4, columnspan = 5)
+        self.inputfield.grid(row = 4, column = 0, sticky = "NSEW", columnspan = 3)
+        self.filebutton.grid(row = 4, column  = 4)
+        self.inputfield.focus_set()
+        self.window.grid_columnconfigure(0,weight = 1)
+        self.window.grid_rowconfigure(0,weight = 1)
+        self.messagedict = {}
+
+    def close(self):
+        try:
+            self.disconnect()
+        except Exception as e:
+            print (e)
+        self.master.destroy()
+
+    
+    def sendinput(self,event):
+        if self.inputfield["fg"]!="grey60":
+            message = self.inputfield.get()
+            self.inputfield.delete(0,tk.END)
+        else:
+            message = ""
+        if message:
+            self.send(self.parsestring(message))
+    
+    def reciveloop(self):
+        try:
+            while True:
+                msg = self.recive()
+                msg = self.decode(msg)
+                if "message" in msg and len(msg["message"])>0:
+                    if msg["message"][0] == CLIENTPREF :
+                        if msg["message"][1:]== "Disconnect":
+                            break
+                    line = f'{msg["sender"]}> {msg["message"]}'
+                    if "clientcommand" in msg and msg["clientcommand"] =="edit":
+                        indexs = messagedict[msg["sender"]+msg["cargs"]]
+                        self.messages.config(state= tk.NORMAL)
+                        self.messages.delete(*indexs)
+                        self.messages.config(state= tk.DISABLED)
+                    else:
+                        if "time" in msg:
+                            first = self.messages.index(tk.END)
+                            last = ".".join((first.split(".")[0],str(int(first.split(".")[1])+len(line))))
+                            self.messagedict[msg["sender"]+str(msg["time"])]= (first,last)
+                        self.messages.config(state= tk.NORMAL)
+                        self.messages.insert(tk.END,line+'\n')
+                        self.messages.see(tk.END)
+                        self.messages.config(state= tk.DISABLED)
+        except Exception as e:
+            raise e
+            self.close()
+            
+    
+    def createfillertext(self,event):
+        if not self.inputfield.get():
+            self.inputfield.config(fg = "grey60")
+            self.inputfield.insert(0,"message server")
+    def removefillertext(self,event):
+        if self.inputfield["fg"] == "grey60":
+            self.inputfield.delete(0,tk.END)
+            self.inputfield.config(fg = "SystemWindowText")
+    
+    def sendfile(self):
+        file = filedialog.askopenfilename()
+        if file:
+            filestream = streamhost(self.master,clientconnection.sendfile,file)
+            self.send({"clientcommand":"stream","ckwargs":{"ip":filestream.ip,"port":filestream.port}})
+
+def main():
+    root = tk.Tk()
+    global cli
+    cli = ui(root,ADRS)
+    recivethread = threading.Thread(target = cli.reciveloop)
+    recivethread.start()
+    root.mainloop()
+
+lemetype = threading.Thread(target = main)
+lemetype.start()
+
---
+++ server.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python3
+from sys import argv, modules
+if 'idlelib.run' in modules: #if the file is runs from idle it runs as a pyw
+    from subprocess import call
+    print ("by defult .pyws are blocked by the firewall, opening as a .py")
+    call(f"python {argv[0]}")
+
+import socket
+import threading
+import json
+import ssl
+import functools
+
+HEADERSIZE = 16
+BUFFERSIZE = 64
+ENCODING = "utf-8"
+SERVERIP = "0.0.0.0"
+PORT = 5050
+ADRS = (SERVERIP,PORT)
+
+SERVERPREF = "/"
+CLIENTPREF = "!"
+FILEPREF = "%"
+
+ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ctx.load_cert_chain("servercert.pem")
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server= ctx.wrap_socket(server,server_side = True)
+server.bind(ADRS)
+server.listen(3)
+print(f"server live on {socket.gethostname()}")
+
+
+class aliasdict(dict): #will only work with strings as keys and is case insensetive
+    def __init__(self,*args,**kwargs):
+        dict.__init__(self,*args,**kwargs)
+        self.aliases = dict()
+    def getkey(self,alias):
+        return self.aliases.get(alias,alias)
+
+    def __getitem__(self,key):
+        key = key.lower()
+        return dict.__getitem__(self ,self.getkey(key))
+
+    def __setitem__(self,key,value):
+        key = key.lower()
+        return dict.__setitem__(self,self.getkey(key),value)
+
+    def __contains__(self,key):
+        key = key.lower()
+        return dict.__contains__(self,self.getkey(key))
+    
+    def pop(self,key,*args,**kwargs):
+        key = key.lower()
+        return dict.pop(self,self.getkey(key),*args,**kwargs)
+    
+    def addalias (self,key,alias):
+        key = key.lower()
+        alias = alias.lower()
+        self.aliases[alias] = key
+    
+    def addaliases(self,key,aliases):
+        for alias in aliases:
+            self.addalias(key,alias)
+
+    def addaliasesfromdict(self,dic):
+        for key in dic:
+            self.addaliases(key,dic[key])
+    
+    def __delitem__(self,key):
+        key = key.lower()
+        print("deleating",key)
+        for i in self.aliases:
+            if self.aliases[i] ==key:
+                self.aiases.pop(i)
+        return dict.__delitem__(self,self.getkey(key))
+
+    def delalias(self,alias):
+        alias = alias.lower()
+        return self.aliuses.pop(alias)
+
+    def renamekey(self,oldkey, newkey):
+        oldkey = oldkey.lower()
+        newkey = newkey.lower()
+        self.__setitem__(newkey,self.pop(self.getkey(oldkey)))
+        for i in self.aliases:
+            if self.aliases[i] ==oldkey:
+                self.aliases[i] = newkey
+                dict.value
+    
+    def renamealias(self,oldalias,newalias):
+        self.alias[newalias] = self.alias.pop(oldalias)
+
+class servercommand(object):
+    cmds = aliasdict()
+    def __init__(self,func,*args,**kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.name = self.func.__name__
+        if "name" in self.kwargs:
+            self.name = self.kwargs["name"]
+        self.cmds[self.name]=self
+        if "alias" in self.kwargs:
+            self.cmds.addalias(self.name, kwargs["alias"])
+        if 'aliases' in self.kwargs:
+            self.cmds.addaliases(self.name,kwargs['aliases'])
+        functools.update_wrapper(self, func)
+        
+    def __call__(self,*fargs,**fkwargs):
+        return(self.func(*fargs,**fkwargs))
+
+
+        
+def servercmd(*args,**kwargs):# this is wrapper classes, just dont think about it too hard
+    def wrapper(func):
+        class servercommandsp(servercommand):
+            @functools.wraps(func)
+            def __call__(self,*fargs,**fkwargs):
+                return(self.func(*fargs,**fkwargs))
+        return servercommandsp(func,*args,**kwargs)
+    return wrapper
+
+
+
+
+class user(object):
+    userdic = aliasdict()
+    def __init__(self,socket,address,username = None):
+        self.socket = socket
+        self.address = address
+        if not username:
+            s = servermessager()
+            s.send("Server connected, please enter username",self)
+            username = self.decode(self.recive())["message"]
+        while True:
+            if not (username in self.userdic or username=="Server") :
+                break
+            s.send("Sorry username taken\n Please select a new one",self)
+            username = self.decode(self.recive())["message"]
+        self.userdic[username] = self
+        self.username = username
+        s.sendall(f"{username} has joined the server")
+    
+    def recive(self):
+        header = self.recivebytes(HEADERSIZE)
+        msg =self.recivebytes(int(header.decode(ENCODING)))
+        return msg
+    
+    def recivebytes(self,msgsize):
+            msgsize = int(msgsize)
+            fullmsg =  b''
+            if msgsize <HEADERSIZE:
+                fullmsg = self.socket.recv(msgsize)
+            else:
+                for i in range(int(msgsize/HEADERSIZE)):
+                    fullmsg += self.socket.recv(HEADERSIZE)
+                fullmsg += self.socket.recv(msgsize%HEADERSIZE)
+            return fullmsg
+    
+    def send(self,msg,reciver):
+        if type(msg) == str:
+            msg = {"message":msg, "sender":self.username}
+        elif type (msg) == dict:
+            msg["sender"] = self.username
+        msg = self.encode(json.dumps(msg))
+        reciver.socket.send(msg)
+    
+    def sendall(self,msg):
+        if type(msg) == str:
+            msg = {"message":msg, "sender":self.username}
+        elif type (msg) == dict:
+            msg["sender"] = self.username
+        msg = self.encode(json.dumps(msg))
+        for reciver in self.userdic.values():
+            reciver.socket.send(msg)
+    
+    @classmethod
+    def encode(cls,msg):
+        msg = bytes(msg,ENCODING)
+        while True:
+            header = bytes(f"{len(msg):<{HEADERSIZE}}",ENCODING)
+            if len(header) > HEADERSIZE:
+                header = cls.encode(json.dumps({"largefile":header}),ENCODING)
+            return header + msg
+    
+    def decode(self,msg):
+        msg = msg.decode(ENCODING)
+        msg = json.loads(msg)
+        if "servercommand" in msg:
+            stri = msg["servercommand"]
+            if stri in servercommand.cmds:
+                cmd = servercommand.cmds[stri]
+                result = cmd(self,*(msg["sargs"]or []),**(msg["skwargs"]or {}))#sargs will alwasy exist but may be equal to None
+            else:
+                s = servermessager()
+                s.send("command not recognised",self)
+                result = ""
+            result = result or "/"+stri+" "+" ".join((msg["sargs"]or []))
+            return result 
+        else:
+            return msg
+    
+    @servercmd (aliases = ("leave"))
+    def disconnect(self):
+        self.userdic.pop(self.username)
+        s = servermessager()
+        s.send({"clientcommand":"Disconnect","ccommandargs":(),"ccommandkwargs":{}},self)
+        self.socket.close()
+        s.sendall(f"{self.username} has left")
+        print("Closed conection from",self.address)
+        return SERVERPREF+"Disconnect"
+
+    @servercmd()
+    def addalias (self,key, alias):
+        servercmds.addalias(key, alias)
+
+    @servercmd(alias = "dm")
+    def messageuser (self, user, *message):
+        user = self.userdic[user]
+        message = " ".join(message)
+        msg = encode(f"{self.username}(private)>{message}")
+        user.socket.send(msg)
+
+    @servercmd(aliases = ["cn","rename"])
+    def changeusername (self,newusername):
+        if newusername in self.userdic and self.username.lower() != newusername.lower():
+            send("userame taken",self.socket)
+        else:
+            self.userdic.renamekey(self.username,newusername)
+            self.username = newusername
+    
+    @servercmd(alias = "cxn")
+    def changexusername (self,otheruser,newusername):
+        self.userdic[otheruser].changeusername(newusername)
+
+    @servercmd()
+    def kick(self,otheruser,*reason):
+        self.userdic[otheruser].disconnect(self.userdic[otheruser])
+        sendall(f'{self.username} kicked {otheruser} "{"".join(reason)}"')
+
+class servermessager(user):
+    def __init__(self):
+        self.username = "Server"
+
+def acceptusers():
+    while True:
+        clientsocket, address = server.accept()
+        print(f"Connection from {address} has been established.")
+        clientthread = threading.Thread(target = handleclient,args = (clientsocket,address))
+        clientthread.start()    
+
+def handleclient(client,address):
+    client = user(client,address)
+    try:
+        message = ""
+        while message!= SERVERPREF+"Disconnect":
+            message = client.recive()
+            message = client.decode(message)
+            client.sendall(message)
+    except Exception as e:
+        client.disconnect(client)
+        raise e
+
+
+
+def sendloop():
+    while True:
+        message = input()
+        if message:
+            sendall(f"Server>{message}")
+
+acloop = threading.Thread(target = acceptusers)
+#sendthread =threading.Thread(target = sendloop)
+acloop.start()
+#sendthread.start()
+
